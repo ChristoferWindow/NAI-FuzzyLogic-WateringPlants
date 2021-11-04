4ece02e26b3ffa37098a19c7abc2261de43e4aed
Krystian <versus146pl@gmail.com>
2021-11-04 20:25:29 +0100

    init

--- FuzzyPlantWatering.py
+++ FuzzyPlantWatering.py
@@ -8,29 +8,24 @@ temperature = ctrl.Antecedent(np.arange(0, 40, 1), 'temperature')
 water_need = ctrl.Antecedent(np.arange(0, 1000, 1), 'water_need')
 water_amount = ctrl.Consequent(np.arange(0, 1000, 1), 'water_amount')
 
-humidity['poor'] = fuzz.trimf(water_amount.universe, [0, 0, 33])
-humidity['average'] = fuzz.trimf(water_amount.universe, [0, 33, 66])
-humidity['high'] = fuzz.trimf(water_amount.universe, [33, 66, 100])
+water_need.automf(3)
+water_amount.automf(3)
 
-temperature['freezing'] = fuzz.trimf(water_amount.universe, [0, 0, 5])
-temperature['cold'] = fuzz.trimf(water_amount.universe, [0, 5, 15])
-temperature['average'] = fuzz.trimf(water_amount.universe, [5, 15, 23])
-temperature['warm'] = fuzz.trimf(water_amount.universe, [15, 23, 33])
-temperature['hot'] = fuzz.trimf(water_amount.universe, [23, 33, 40])
+humidity['dry'] = fuzz.trimf(humidity.universe, [0, 0, 33])
+humidity['optimal'] = fuzz.trimf(humidity.universe, [0, 33, 66])
+humidity['humid'] = fuzz.trimf(humidity.universe, [33, 66, 100])
 
-water_need['low'] = fuzz.trimf(water_amount.universe, [0, 0, 333])
-water_need['medium'] = fuzz.trimf(water_amount.universe, [0, 333, 666])
-water_need['high'] = fuzz.trimf(water_amount.universe, [333, 666, 1000])
+temperature['freezing'] = fuzz.trimf(temperature.universe, [0, 0, 5])
+temperature['cold'] = fuzz.trimf(temperature.universe, [0, 5, 15])
+temperature['optimal'] = fuzz.trimf(temperature.universe, [5, 15, 23])
+temperature['warm'] = fuzz.trimf(temperature.universe, [15, 23, 33])
+temperature['hot'] = fuzz.trimf(temperature.universe, [23, 33, 40])
 
-water_amount['low'] = fuzz.trimf(water_amount.universe, [0, 0, 333])
-water_amount['medium'] = fuzz.trimf(water_amount.universe, [0, 333, 666])
-water_amount['high'] = fuzz.trimf(water_amount.universe, [333, 666, 1000])
-
-rule1 = ctrl.Rule(humidity['poor'] | water_need['low'], water_amount['medium'])
-rule2 = ctrl.Rule(water_need['average'], water_amount['medium'])
-rule3 = ctrl.Rule(temperature['warm'] | water_need['high'], water_amount['high'])
-rule4 = ctrl.Rule(humidity['poor'] | temperature['hot'], water_amount['high'])
-rule5 = ctrl.Rule(water_need['high'] | temperature['average'] | humidity['high'], water_amount['medium'])
+rule1 = ctrl.Rule(humidity['dry'] | water_need['poor'], water_amount['average'])
+rule2 = ctrl.Rule(water_need['average'], water_amount['average'])
+rule3 = ctrl.Rule(temperature['warm'] | water_need['good'], water_amount['good'])
+rule4 = ctrl.Rule(humidity['dry'] | temperature['hot'], water_amount['good'])
+rule5 = ctrl.Rule(water_need['good'] | temperature['optimal'] | humidity['humid'], water_amount['average'])
 
 water_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5])
 
@@ -40,9 +35,9 @@ water.input['humidity'] = 77
 water.input['temperature'] = 23
 water.input['water_need'] = 450
 
-water.compte()
+water.compute()
 
-print(water.output['water'])
-water.view(sim=water)
+print(water.output['water_amount'])
+water_amount.view(sim=water)
 
 plt.show()
